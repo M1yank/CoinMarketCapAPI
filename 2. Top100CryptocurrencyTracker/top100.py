d45066027e7cac8bb80514fdd09781d03b6631f0
M1yank <mbansal1_be19@thapar.edu>
2022-09-02 01:52:10 +0530

    main script

---
+++ 2. Top100CryptocurrencyTracker/top100.py
@@ -0,0 +1,104 @@
+# from locale import currency
+import os
+import json
+from urllib import request
+import requests
+from datetime import datetime
+from prettytable import PrettyTable
+from colorama import Fore, Back, Style
+from dotenv import load_dotenv
+local_currency = 'USD'
+local_currency_symbol = '$'
+
+load_dotenv()
+api_key = os.getenv('api_key')
+headers = {'X-CMC_PRO_API_KEY': api_key}
+
+base_url = 'https://pro-api.coinmarketcap.com'
+
+
+print()
+print("Top 100 Cryptocurrenct Ranker:")
+print("1 - Top 100 sorted by market cap")
+print("2 - Top 100 sorted by 24H percent Change")
+print("3 - Top 100 sorted by 24H Volume")
+print("0 - EXIT!!!")
+i = int(input('What is your choice (1-3)???  '))
+
+sort = ""
+if(i==int(1)):
+    sort = 'market_cap'
+elif(i==int(2)):
+    sort = 'percent_change_24h'
+elif(i==int(3)):
+    sort = 'volume_24h'
+elif(i==int(0)):
+    exit(0)
+
+quote_url = base_url + '/v1/cryptocurrency/listings/latest?convert='+local_currency+'&sort='+sort
+request = requests.get(quote_url, headers = headers)
+results = request.json()
+
+# print(json.dumps(results, sort_keys=True, indent = 4))
+
+data = results['data']
+table = PrettyTable(['Asset', 'Price', 'Market Cap', 'Volume', '1H', '24H', '7D'])
+
+print()
+
+for currency in data:
+    name = currency['name']
+    symbol = currency['symbol']
+
+    quote = currency['quote'][local_currency]
+    market_cap = quote['market_cap']
+    hour_change = quote['percent_change_1h']
+    day_change = quote['percent_change_24h']
+    week_change = quote['percent_change_7d']
+    price = quote['price']
+    volume = quote['volume_24h']
+
+    if hour_change is not None:
+        hour_change = round(hour_change, 2)
+        if hour_change > 0:
+            hour_change = Back.GREEN + str(hour_change) + '%' + Style.RESET_ALL
+        else:
+            hour_change = Back.RED + str(hour_change) + '%' + Style.RESET_ALL
+
+    if day_change is not None:
+        day_change = round(day_change, 2)
+        if day_change > 0:
+            day_change = Back.GREEN + str(day_change) + '%' + Style.RESET_ALL
+        else:
+            day_change = Back.RED + str(day_change) + '%' + Style.RESET_ALL
+        
+    if week_change is not None:
+        week_change = round(week_change, 2)
+        if week_change > 0:
+            week_change = Back.GREEN + str(week_change) + '%' + Style.RESET_ALL
+        else:
+            week_change = Back.RED + str(week_change) + '%' + Style.RESET_ALL
+
+    if volume is not None:
+        volume_string = '{:,}'.format(round(volume, 2))
+    
+    if market_cap is not None:
+        market_cap_string = '{:,}'.format(round(market_cap, 2))
+        
+    
+    price_string = '{:,}'.format(round(price,2))
+
+    table.add_row([name + ' (' + symbol + ')',
+            local_currency_symbol + price_string,
+            local_currency_symbol + market_cap_string,
+            local_currency_symbol + volume_string,
+            str(hour_change),
+            str(day_change),
+            str(week_change)])
+
+print()
+print(table)
+print()
+
+    
+    
